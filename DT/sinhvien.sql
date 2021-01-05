770c91e3cce74114513853de3eaff95f862aeb48
hungminhle10072000 <57429082+hungminhle10072000@users.noreply.github.com>
2021-01-05 17:20:21 +0700

    new

--- DT/sinhvien.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `SINHVIEN` (
-  `ma_SV` int(11) NOT NULL,
-  `ten_SV` varchar(250) NOT NULL,
-  `ngay_Sinh` date NOT NULL,
-  `ten_Lop` varchar(250) NOT NULL,
-    `ten_Khoa` varchar(250) NOT NULL,
-  PRIMARY KEY (`ma_SV`)
-)
