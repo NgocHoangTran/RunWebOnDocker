c81843421dddc3d41d0c6f0712b423a663b66dfe
hungminhle10072000 <57429082+hungminhle10072000@users.noreply.github.com>
2021-01-05 17:21:50 +0700

    new

---
+++ Spring-boot/sinhvien.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `SINHVIEN` (
+  `ma_SV` int(11) NOT NULL,
+  `ten_SV` varchar(250) NOT NULL,
+  `ngay_Sinh` date NOT NULL,
+  `ten_Lop` varchar(250) NOT NULL,
+    `ten_Khoa` varchar(250) NOT NULL,
+  PRIMARY KEY (`ma_SV`)
+)
